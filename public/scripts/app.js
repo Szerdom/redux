0fc548b0e960439e849c8f5c60a7ff9acf2c419a
Szerdom <szeremetadom@gmail.com>
2018-06-17 19:30:31 +0200

    home connect

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,63 @@
-"use strict";
+'use strict';
 
 console.log("Hi!");
 
-var newTemplate = React.createElement(
-    "div",
+var app = {
+    title: 'Title',
+    subtitle: 'Subtitle',
+    options: ['One', 'Two']
+};
+
+var firstTemplate = React.createElement(
+    'div',
     null,
     React.createElement(
-        "h1",
+        'h1',
         null,
-        "Dominik"
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
     ),
     React.createElement(
-        "p",
+        'p',
         null,
-        "Age: 24"
-    ),
-    React.createElement(
-        "p",
-        null,
-        "Location: GDA"
+        app.options && app.options.length > 0 ? "Here you are" : "None"
     )
+);
+var getLocation = function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+};
+
+var user = {
+    name: 'Dominik',
+    age: 24
+    //location: 'GDA'
+};
+var newTemplate = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Dominik'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 24'
+    ),
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(newTemplate, appRoot);
+ReactDOM.render(firstTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
 console.log("Hi!");
 
+var app = {
+    title: 'Title',
+    subtitle: 'Subtitle',
+    options: ['One', 'Two']
+};
+
+var firstTemplate = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{(app.options && app.options.length > 0) ? "Here you are" : "None"}</p>
+    </div>
+);
+let getLocation = (location) => {
+    if(location)
+    {
+        return <p>Location: {location}</p>;
+    }
+};
+
+let user = {
+    name: 'Dominik',
+    age: 24,
+    //location: 'GDA'
+};
 var newTemplate = (
     <div>
         <h1>Dominik</h1>
         <p>Age: 24</p>
-        <p>Location: GDA</p>
+        {getLocation(user.location)}
     </div>
 );
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(newTemplate, appRoot);
+ReactDOM.render(firstTemplate, appRoot);
