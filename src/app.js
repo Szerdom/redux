06ad52e41044a7f9bd0e214f8475953f2c632060
Dominik Szeremeta <szeremetadom@gmail.com>
2018-07-11 18:00:23 +0200

    refactoring & hoc start

--- src/app.js
+++ src/app.js
@@ -3,5 +3,22 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', note: 'Water bill', amount: 10 }));
+store.dispatch(addExpense({ description: 'Gas bill', note: 'Gas bill', amount: 10 }));
+
+store.dispatch(setTextFilter('elko'));
+
+const state = store.getState();
+const vExp = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(vExp);
+//console.log(getVisibleExpenses());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
