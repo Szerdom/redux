50f476d81fdcfabc877b7dc56cc9669088481d17
Dominik Szeremeta <szeremetadom@gmail.com>
2018-07-23 17:55:04 +0200

    Enzyme Added

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,10 +3,15 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     <div>
-        <h1>ExpenseList</h1>
-        {props.expenses.map((expense) => <ExpenseListItem {...expense} />)}
+        {
+            props.expenses.length === 0 ? (
+                <p>No expenses</p>
+            ) : (
+                props.expenses.map((expense) => <ExpenseListItem {...expense} />)
+            )
+        }
     </div>
 );
 
