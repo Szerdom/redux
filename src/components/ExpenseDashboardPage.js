5c3025ed54c3f377dbf2c17292d13cbf47e20ac9
Szerdom <szeremetadom@gmail.com>
2018-07-29 17:53:59 +0200

    Summary with test

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-import selectExpenses from '../selectors/expenses';
-import selectExpensesTotal from '../selectors/expenses-total';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = (props) => (
     <div>
-        <p>{selectExpensesTotal(props.expenses)}</p>
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
 
-const mapStateToProps = (state) => {
-    return {
-        expenses: selectExpenses(state.expenses, state.filters)
-    };
-}
-
-export default connect(mapStateToProps)(ExpenseDashboardPage);
+export default ExpenseDashboardPage;
