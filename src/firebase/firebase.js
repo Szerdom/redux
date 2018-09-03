e24c61842520358b706d7d459679696e57c74047
Dominik Szeremeta <szeremetadom@gmail.com>
2018-09-03 17:28:26 +0200

    Setup env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import moment from 'moment';
 import * as expensesActions from '../actions/expenses';
 
 const config = {
-    apiKey: "AIzaSyB_HYH3uAICRV7nqmhdWIRubJXHU0uYih8",
-    authDomain: "redux-szerdom.firebaseapp.com",
-    databaseURL: "https://redux-szerdom.firebaseio.com",
-    projectId: "redux-szerdom",
-    storageBucket: "redux-szerdom.appspot.com",
-    messagingSenderId: "785824269519"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
